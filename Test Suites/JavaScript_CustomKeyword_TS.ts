b7b0ca24a01a4c36298f949973f3d90f325bff03
linh.nguyen <linh.nguyen@10.10.32.67>
2025-09-11 14:30:26 +0700

    TS Custom Keyword

---
+++ Test Suites/JavaScript_CustomKeyword_TS.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>JavaScript_CustomKeyword_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c2275c7a-b0d4-4e37-b145-cf1f471dc1a4</testSuiteGuid>
+   <testCaseLink>
+      <guid>f648978a-7796-49cb-b7f2-adb8c427889b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SelectOption_CustomKeywords_Sample_TC</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>35bce8e7-4836-4b8b-9d76-7ec0fec7c031</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SetText_CustomKeywords_Sample_TC</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
