3f3fc479a7a35178ec1aac0f3571d17a8326c5dc
linh.nguyen <linh.nguyen@192.168.1.6>
2025-09-16 13:27:22 +0700

    Verify Text Failed TS

---
+++ Test Suites/VerifyText_Failed_TS.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>VerifyText_Failed_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f658de6d-41b1-4b14-94ec-8d815e9878bb</testSuiteGuid>
+   <testCaseLink>
+      <guid>cd7edf6b-c704-44fe-b0ce-e73baf93a30f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyText_Failed_TC</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
